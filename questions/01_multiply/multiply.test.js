ca9a6cf08c8c3c8d8316eec3ab7e22b3fa6cd992
Caroline B Flanagan <carolineflan@gmail.com>
2020-04-25 09:49:22 -0400

    multiply solution

--- questions/01_multiply/multiply.test.js
+++ questions/01_multiply/multiply.test.js
@@ -4,23 +4,24 @@ describe('multiply testing', () => {
   test('it should ensure it receives two numbers', () => {
     // test some values that should fail
 
-    expect(() => multiply(/*fill this in*/)).toThrow();
+    expect(() => multiply(NaN)).toThrow('error');
 
-    expect(() => multiply(/*fill this in*/)).toThrow();
+    expect(() => multiply('string')).toThrow('error');
   });
 
   test('it returns the product of two numbers', () => {
     // test the type of the returned value
-    expect(/*fill this in*/).toBe(/*fill this in*/);
+    expect(8 * 5).toBe(40);
 
     // test that the returned value is correct
-    expect(/*fill this in*/).toBe(/*fill this in*/);
+    expect(6 * 10).toBe(60);
 
     // test some other values
-    expect(/* fill this in */).toBe(/* fill this in */);
+    expect(400 * 3).toBe(1200);
   });
 
   test('it is not hardcoded (hint: use random numbers)', () => {
     // test some random values
+    Math.random() * Math.random() = Math.random() * Math.random();
   });
 });
