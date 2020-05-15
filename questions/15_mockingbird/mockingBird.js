244027b29e1e2260d5b81fb38d6dc48902fb3163
Caroline B Flanagan <carolineflan@gmail.com>
2020-05-15 01:01:24 -0400

    stretch15

--- questions/15_mockingbird/mockingBird.js
+++ questions/15_mockingbird/mockingBird.js
@@ -1,6 +1,11 @@
 // repeater runs a function n times and returns an array of its outputs
 const repeater = (func, n) => {
   // YOUR CODE
+  let newArr = [];
+  for (let i = 0; i < n; i++) {
+    newArr.push(func(i));
+  }
+  return newArr;
 };
 
 // see test specs to complete this stretch
