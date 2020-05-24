7465fe5cc223ca6ca01a584fae7fbe6d92591387
Caroline B Flanagan <carolineflan@gmail.com>
2020-05-23 23:20:37 -0400

    stretch23

--- questions/23_extension/extension.js
+++ questions/23_extension/extension.js
@@ -1,5 +1,11 @@
 //write a function that takes a function, and makes it callable with two invocations
 
-const extensions = () => {};
+const extensions = (cb, num1, num2) => {
+    if (!cb || !num1 | !num2){
+        throw 'Error';
+    } else {
+        cb(num1, num2);
+    }
+};
 
 module.exports = { extensions };
